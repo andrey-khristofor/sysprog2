e46db901b2f4d0689dc3eb671047239330ee178f
andrey-khristofor <72157417+andrey-khristofor@users.noreply.github.com>
2021-11-23 10:43:28 +0200

    Add files via upload

---
+++ Lab2/src/DFAState.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class DFAState {
+    ArrayList<Integer> startStates, endStates;
+    String symbol;
+
+    DFAState(ArrayList<Integer> startStates, String symbol, ArrayList<Integer> endStates){
+        this.startStates = startStates;
+        this.symbol = symbol;
+        this.endStates = endStates;
+    }
+
+
+    public boolean equalToStart(ArrayList<Integer> find){
+        int matches = 0;
+        if(find.size() != startStates.size()) return false;
+        for(int g=0; g< find.size(); ++g){
+            if(startStates.contains(find.get(g))) matches++;
+        }
+        return matches == find.size();
+    }
+
+}
